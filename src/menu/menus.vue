9d1719d33e5998a672f3f6cdca2859d0a2d1fd1e
xc9010 <xuchen199010@gmail.com>
2020-04-20 00:49:40 +0800

    add

--- src/menu/menus.vue
+++ src/menu/menus.vue
@@ -13,7 +13,7 @@
           <span>业务操作</span>
         </template>
         <el-menu-item-group>
-          <template slot="title">分组一</template>
+          <template slot="title">业务一</template>
           <el-menu-item index="1-1">选项1</el-menu-item>
           <el-menu-item index="1-2">选项2</el-menu-item>
         </el-menu-item-group>
@@ -27,11 +27,11 @@
       </el-submenu>
       <el-menu-item index="2">
         <i class="el-icon-menu"></i>
-        <span slot="title">导航二</span>
+        <span slot="title">业务二</span>
       </el-menu-item>
       <el-menu-item index="3" disabled>
         <i class="el-icon-document"></i>
-        <span slot="title">导航三</span>
+        <span slot="title">业务三</span>
       </el-menu-item>
       <el-menu-item index="4">
         <i class="el-icon-setting"></i>
