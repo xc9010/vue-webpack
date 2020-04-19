11bf544c5f4fc78310f1858b5eb7360a6ab8015a
xuchen <xuchen@shein.com>
2020-04-20 00:37:53 +0800

    add

--- src/menu/menus.vue
+++ src/menu/menus.vue
@@ -39,3 +39,16 @@
       </el-menu-item>
     </el-menu>
 </template>
+
+<script>
+  export default {
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
